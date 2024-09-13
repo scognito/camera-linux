76be96194d88ef9b9cbb86c5627c784f3b1ac637
Pietro Di Vita <p.divita@saltosystems.com>
2024-09-13 13:49:07 +0000

    created function takePicutre

--- lib/camera_linux.dart
+++ lib/camera_linux.dart
@@ -1,9 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:typed_data';
+import 'package:cross_file/cross_file.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'camera_linux_bindings_generated.dart';
 import 'package:ffi/ffi.dart';
+import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraLinux {
   late CameraLinuxBindings _bindings;
@@ -21,6 +27,32 @@ class CameraLinux {
   // Close The Camera
   void stopCamera() {
     _bindings.stopVideoCapture();
+  }
+
+  // Take a Picture and return XFile
+  Future<XFile> takePicture() async {
+    final Uint8List imageBytes = await getImageData();
+    final base64String = uint8ListToBase64Url(imageBytes);
+
+    // Convert Base64 to Image File
+    final imageData = base64Decode(base64String);
+
+    final fileName = 'picture_${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+    final directory = await getTemporaryDirectory();
+    final filePath = '${directory.path}/$fileName';
+
+    final file = File(filePath);
+    await file.writeAsBytes(imageData);
+
+    return XFile(filePath);
+  }
+
+  // Get Image Bytes
+  Future<Uint8List> getImageData() async {
+    final lengthPtr = calloc<Int>();
+    Pointer<Uint8> framePointer = _bindings.getLatestFrameBytes(lengthPtr);
+    return getLatestFrameData(framePointer, lengthPtr.value);
   }
 
   // Convert Frame Into Base64
